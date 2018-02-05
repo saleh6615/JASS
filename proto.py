4dc25080b8bf121e59c31cf54f4cc0b8c30a0c9a
saleh <saleh.asadallahi@gmail.com>
2018-02-05 00:05:57 -0600

    Add lock and unlock functionality with threads and stepper motor

--- proto.py
+++ proto.py
@@ -1,12 +1,13 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 
 red = 11
 green = 13
 switch = 15
 A0, A1, B0, B1  = [29, 31, 33, 35]
 GPIO.setmode(GPIO.BOARD)
-
+threads = []
 def init():
 	GPIO.setup([red, green], GPIO.OUT)	
 	GPIO.setup([A0, A1, B0, B1], GPIO.OUT)
@@ -19,6 +20,8 @@ def flash(light):
 def motor_delay():
     time.sleep(.01)
 def motor_unlock():
+    time.sleep(.5)
+    for _ in range(25):
         GPIO.output([A1, B1], GPIO.LOW)
         GPIO.output([A0, B0], GPIO.HIGH)
         motor_delay()
@@ -31,15 +34,41 @@ def motor_unlock():
         GPIO.output([A1, B0], GPIO.LOW)
         GPIO.output([A0, B1], GPIO.HIGH)
         motor_delay()
+def motor_lock():
+    time.sleep(.5)
+    for _ in range(25):
+        GPIO.output([A1, B0], GPIO.LOW)
+        GPIO.output([A0, B1], GPIO.HIGH)
+        motor_delay()
+        GPIO.output([A0, B0], GPIO.LOW)
+        GPIO.output([A1, B1], GPIO.HIGH)
+        motor_delay()
+        GPIO.output([A0, B1], GPIO.LOW)
+        GPIO.output([A1, B0], GPIO.HIGH)
+        motor_delay()
+        GPIO.output([A1, B1], GPIO.LOW)
+        GPIO.output([A0, B0], GPIO.HIGH)
+        motor_delay()
 def c_unlock():
-	flash(red)
-        for _ in range(50):
-            motor_unlock()
+        t1 = threading.Thread(target = flash, args = (red, ))
+        t2 = threading.Thread(target = motor_unlock)
+        #threads.append(t1)
+        #threads.append(t2)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
 	GPIO.output(red, GPIO.LOW)
 	time.sleep(.5)
 	GPIO.output(green, GPIO.HIGH)
 def c_lock():
-	flash(green)
+        t1 = threading.Thread(target = flash, args = (green, ))
+        t2 = threading.Thread(target = motor_lock)
+	#flash(green)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
 	GPIO.output(green, GPIO.LOW)
 	time.sleep(.5)
 	GPIO.output(red, GPIO.HIGH)
